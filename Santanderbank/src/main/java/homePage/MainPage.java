6652644529ba934a326472bf1d52dac09fefd5ab
MdManiruzzaman <159manir@gmail.com>
2019-01-06 20:32:09 -0500

    modified Santanderbank

--- Santanderbank/src/main/java/homePage/MainPage.java
+++ Santanderbank/src/main/java/homePage/MainPage.java
@@ -13,6 +13,10 @@ public class MainPage extends BaseUtil {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         viewWareHouseSavings.click(); //example need to fix
     }
+    public void checkviewWareHouseSings(){ //example need to fix
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        viewWareHouseSavings.click(); //example need to fix
+    }
 
 
 
